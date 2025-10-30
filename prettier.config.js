eee56928f2a40a7b104f013773e6824488f14f30
SeHyun <rla6170@naver.com>
2025-10-30 13:43:36 +0900

    chore(prettier): configure prettier setting

---
+++ prettier.config.js
@@ -0,0 +1,17 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  endOfLine: "lf",
+  bracketSameLine: false,
+  arrowParens: "avoid",
+  bracketSpacing: true,
+  printWidth: 120,
+};
+
+export default config;
