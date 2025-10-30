0647956ca7036caee7f6ff58199a6f2332a8f407
SeHyun <rla6170@naver.com>
2025-10-30 14:49:28 +0900

    chore(eslint): fix configuration eslint plugin prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,12 @@
-import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
-import eslintConfigPrettier from "eslint-config-prettier";
+import eslintConfigPrettierRecommended from "eslint-config-prettier/prettier";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  eslintConfigPrettier,
+  ...eslintConfigPrettierRecommended,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
