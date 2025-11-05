1c442641f669b5ffcbcb50a9880cfc9f18aab988
“moming109” <skhu202013029@gmail.com>
2025-11-05 15:33:55 +0900

    feat: hover, disabled css 추가

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -4,37 +4,42 @@ import { cva, VariantProps } from "class-variance-authority";
 import { LucideIcon } from "lucide-react";
 import { twMerge } from "tailwind-merge";
 
-const button = cva("inline-flex items-center justify-center rounded-lg", {
+const button = cva("inline-flex items-center justify-center rounded-lg cursor-pointer", {
   variants: {
     size: {
-      XLarge: "w-[343px] text-base py-4 gap-4",
-      Large: "w-[311px] text-base py-4 gap-3",
-      Medium: "w-[151px] text-sm py-3 gap-2",
-      Small: "w-[79px] text-xs py-2 gap-0",
-      XSmall: "w-[79px] h-fit text-xs py-1.5 gap-0 leading-none",
+      xLarge: "min-w-[343px] text-base py-4 gap-4",
+      large: "min-w-[311px] text-base py-4 gap-3",
+      medium: "min-w-[151px] text-sm py-3 gap-2",
+      small: "min-w-[79px] text-xs py-2 gap-0",
+      xSmall: "min-w-[79px] h-fit text-xs py-1.5 gap-0",
     },
     variant: {
-      "1": "bg-[var(--color-main)] text-white",
-      "2": "border border-[var(--color-bg-sub)] bg-[var(--color-main-50)] text-[var(--color-main)] ",
-      "3": "border border-[var(--color-bg-sub)] bg-[var(--color-bg-main)] text-[var(--color-text-content)]",
-      "4": "border border-[var(--color-main)] bg-[var(--color-bg-main)] text-[var(--color-main)]",
-      "5": "bg-[var(--color-bg-main)] text-[var(--color-main)]",
+      primary: "bg-[var(--color-main)] text-white hover:bg-blue-400 disabled:bg-gray-300",
+      secondary:
+        "border border-[var(--color-bg-sub)] bg-[var(--color-main-50)] text-[var(--color-main)] hover:bg-blue-100 disabled:bg-gray-200 disabled:text-white",
+      tertiary:
+        "border border-[var(--color-bg-sub)] bg-[var(--color-bg-main)] text-[var(--color-text-content)] hover:border-gray-400 disabled:border-gray-300 disabled:text-gray-200 disabled:border-gray-200",
+      quaternary:
+        "border border-[var(--color-main)] bg-[var(--color-bg-main)] text-[var(--color-main)] disabled:text-gray-200 disabled:border-blue-100",
+      quinary: "bg-[var(--color-bg-main)] text-[var(--color-main)] disabled:text-gray-200",
     },
   },
 
   defaultVariants: {
-    size: "Medium",
-    variant: "1",
+    size: "medium",
+    variant: "primary",
   },
 });
 
-type ButtonProps = { Icon?: LucideIcon } & VariantProps<typeof button> & React.ComponentPropsWithoutRef<"button">;
+interface ButtonProps extends React.ComponentPropsWithoutRef<"button">, VariantProps<typeof button> {
+  Icon?: LucideIcon;
+}
 
 export function Button({ size, variant, children, Icon, ...props }: ButtonProps) {
   return (
     <>
       <button className={twMerge(button({ size, variant }))} {...props}>
-        {size !== "Small" && size !== "XSmall" && Icon && <Icon />}
+        {size !== "small" && size !== "xSmall" && Icon && <Icon />}
         {children}
       </button>
     </>
