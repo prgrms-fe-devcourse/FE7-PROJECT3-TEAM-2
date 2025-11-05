53185ca438ad0871234465930c32a6979702003b
“moming109” <skhu202013029@gmail.com>
2025-11-05 15:02:17 +0900

    feat: create Button component

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { cva, VariantProps } from "class-variance-authority";
+import { twMerge } from "tailwind-merge";
+
+const button = cva("inline-flex items-center justify-center rounded-lg", {
+  variants: {
+    size: {
+      XLarge: "w-[343px] text-base py-4 gap-4",
+      Large: "w-[311px] text-base py-4 gap-3",
+      Medium: "w-[151px] text-sm py-3 gap-2",
+      Small: "w-[79px] text-xs py-2 gap-0",
+      XSmall: "w-[79px] h-fit text-xs py-1.5 gap-0 leading-none",
+    },
+    variant: {
+      "1": "bg-[var(--color-main)] text-white",
+      "2": "border border-[var(--color-bg-sub)] bg-[var(--color-main-50)] text-[var(--color-main)] ",
+      "3": "border border-[var(--color-bg-sub)] bg-[var(--color-bg-main)] text-[var(--color-text-content)]",
+      "4": "border border-[var(--color-main)] bg-[var(--color-bg-main)] text-[var(--color-main)]",
+      "5": "bg-[var(--color-bg-main)] text-[var(--color-main)]",
+    },
+  },
+
+  defaultVariants: {
+    size: "Medium",
+    variant: "1",
+  },
+});
+
+type ButtonProps = VariantProps<typeof button> & React.ComponentPropsWithoutRef<"button">;
+
+export function Button({ size, variant, children, ...props }: ButtonProps) {
+  return (
+    <>
+      <button className={twMerge(button({ size, variant }))} {...props}>
+        {children}
+      </button>
+    </>
+  );
+}
