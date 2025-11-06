24efc7a5c7d243e6eb0f4c8d59eb60a19dc69a2a
SeHyun <rla6170@naver.com>
2025-11-06 15:37:33 +0900

    feat: header navigation 연결

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,5 +1,15 @@
 "use client";
 
+import HeaderNav from "@/components/layout/HeaderNav";
+
 export default function Header() {
-  return <header className="h-(--header-height) border-b border-gray-300">Header</header>;
+  return (
+    <header className="flex h-(--header-height) items-center justify-between border-b border-gray-300 bg-white p-(--global-padding)">
+      <div>logo</div>
+      <div>
+        <div className="hidden sm:block"></div>
+        <HeaderNav />
+      </div>
+    </header>
+  );
 }
