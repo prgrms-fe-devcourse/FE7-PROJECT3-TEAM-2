637774814a1550ddd8d6de9518632f074a2a0717
SeHyun <rla6170@naver.com>
2025-11-06 15:36:01 +0900

    fix: nav children 하위 페이지 단위로 변경

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -1,27 +1,35 @@
 "use client";
 
+import { Settings2, ShieldAlert } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { Button } from "@/components/common/Button";
 import { NavButton } from "@/components/common/NavButton";
 import { navData } from "@/routes/routes.data";
 
 export default function Nav() {
+  const pathname = usePathname();
+
+  const isActive = (path: string) => {
+    if (path === "/") return path === pathname;
+    else return pathname.startsWith(path);
+  };
+
   return (
-    <nav className="bg-bg-sub flex flex-row justify-between border border-gray-200 sm:w-[223px] sm:flex-col">
+    <nav className="bg-bg-sub hidden flex-row justify-between overflow-y-scroll border border-gray-200 sm:block sm:h-[calc(100vh-var(--header-height))] sm:w-[223px] sm:flex-col sm:p-6">
       <ul className="flex items-center gap-3 sm:flex-col sm:items-baseline">
         {navData.map(nav => {
           return (
             <li className="flex flex-col sm:w-full" key={nav.pathname}>
-              <NavButton href={nav.pathname} icon_img={nav.icon} variant="main">
+              <NavButton href={nav.pathname} icon_img={nav.icon} variant="main" active={isActive(nav.pathname)}>
                 {nav.label}
               </NavButton>
-              <ul className="hidden sm:block">
+              <ul className="hidden pl-6 sm:block">
                 {nav.children &&
                   nav.children.map(subNav => {
+                    const path = `${nav.pathname}/${subNav.pathname}`;
                     return (
-                      <NavButton
-                        variant="sub"
-                        key={subNav.pathname}
-                        href={{ pathname: nav.pathname, query: { type: subNav.pathname } }}
-                      >
+                      <NavButton variant="sub" key={subNav.pathname} active={isActive(path)} href={{ pathname: path }}>
                         {subNav.label}
                       </NavButton>
                     );
@@ -31,9 +39,19 @@ export default function Nav() {
           );
         })}
       </ul>
-      <ul>
-        <li>설정</li>
-        <li>로그인</li>
+      <ul className="flex flex-row sm:flex-col">
+        <li>
+          <Button variant="secondary">
+            <Settings2 size={16} />
+            설정
+          </Button>
+        </li>
+        <Link href="/login">
+          <Button variant="tertiary">
+            <ShieldAlert size={16} />
+            로그인
+          </Button>
+        </Link>
       </ul>
     </nav>
   );
