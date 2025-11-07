51b33e3798ae48f801d179f280b979630a836a72
SeHyun <rla6170@naver.com>
2025-11-07 12:50:33 +0900

    feat: github 로그인 버튼 연결

--- src/components/auth/SocialLogins.tsx
+++ src/components/auth/SocialLogins.tsx
@@ -1,11 +1,14 @@
+"use client";
+
 import SocialLoginButton from "@/components/auth/SocialLoginButton";
+import { signInWithGithub } from "@/services/auth/login";
 
 export default function SocialLogins() {
   return (
     <div className="flex flex-row justify-center gap-3 sm:flex-col">
       <SocialLoginButton social="naver" disabled />
       <SocialLoginButton social="kakao" />
-      <SocialLoginButton social="github" />
+      <SocialLoginButton social="github" onClick={() => signInWithGithub()} />
       <SocialLoginButton social="google" />
     </div>
   );
