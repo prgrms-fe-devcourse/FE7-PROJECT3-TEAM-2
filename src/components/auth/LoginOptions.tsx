3e87c16252d6f55ee5e1195eb5b4b7d2ce82fb7e
SeHyun <rla6170@naver.com>
2025-11-07 09:16:01 +0900

    feat: 로그인 사용자 옵션(아이디 찾기 비밀번호 찾기 등) UI

---
+++ src/components/auth/LoginOptions.tsx
@@ -0,0 +1,21 @@
+export default function LoginOptions() {
+  return (
+    <div className="flex flex-col items-center gap-2 text-sm font-medium text-gray-400">
+      <div className="flex gap-2">
+        <span>계정이 기억나지 않으신가요?</span>
+        <a href="" className="text-gray-950">
+          이메일 찾기
+        </a>
+        <a href="" className="text-gray-950">
+          비밀번호 찾기
+        </a>
+      </div>
+      <div className="flex gap-2">
+        <span>아직 계정이 없으신가요?</span>
+        <a href="" className="text-gray-950">
+          이메일로 회원가입
+        </a>
+      </div>
+    </div>
+  );
+}
