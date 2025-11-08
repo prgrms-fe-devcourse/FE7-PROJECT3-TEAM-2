981703c51040cb4d7e6b27b0e397f47670e90f36
Song YoungJu <moeirs545@gmail.com>
2025-11-09 05:11:19 +0900

    feat: comment ui 구현 완료

--- src/app/posts/[category]/post/[postId]/page.tsx
+++ src/app/posts/[category]/post/[postId]/page.tsx
@@ -1,3 +1,4 @@
+import { twMerge } from "tailwind-merge";
 import MessageBubble from "@/components/comment/MessageBubble";
 import ResponsiveContainer from "@/components/common/ResponsiveContainer";
 import PostCard from "@/components/post/PostCard";
@@ -23,19 +24,26 @@ export default async function PostPage({ params }: { params: Promise<{ postId: s
   // const commentDataSample = null;
   return (
     <>
-      <ResponsiveContainer className="bg-bg-sub w-full overflow-hidden max-sm:border-none">
+      <ResponsiveContainer className="bg-bg-sub flex w-full flex-col overflow-hidden max-sm:border-none">
         <PostCard
           userName="나"
           title="이거 썸 맞나요?"
           content="이거 썸 맞나요?"
           className="bg-bg-main rounded-t-none border-t-0 border-r-0 border-l-0"
         />
-        <div className="px-6 py-5">
-          {commentDataSample ? (
-            commentDataSample.map(comment => <MessageBubble key={comment.id} data={comment} />)
-          ) : (
-            <p className="text-text-sub text-center text-sm">등록된 댓글이 없습니다.</p>
-          )}
+        <div className="flex h-full flex-col justify-between px-6 py-5">
+          <div className={twMerge("comments flex h-full flex-col", !commentDataSample && "justify-center")}>
+            {" "}
+            {commentDataSample ? (
+              commentDataSample.map(comment => <MessageBubble key={comment.id} data={comment} />)
+            ) : (
+              <p className="text-text-sub text-center text-sm">등록된 댓글이 없습니다.</p>
+            )}
+          </div>
+          <div className="comment-input-container bg-bg-main mt-5 flex items-center rounded-lg px-5 py-4">
+            <input type="text" name="" id="" className="bg-bg-sub mr-3 flex-3/4 rounded-sm px-2 py-1 outline-0" />
+            <button className="bg-main cursor-pointer rounded-sm px-3 py-1.5 text-xs text-white">보내기</button>
+          </div>
         </div>
       </ResponsiveContainer>
     </>
