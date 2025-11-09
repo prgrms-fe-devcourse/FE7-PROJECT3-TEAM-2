209067b65633da4ba9fca7a8e1cdc4fb1f245e47
“moming109” <skhu202013029@gmail.com>
2025-11-10 03:05:47 +0900

    feat: create BadgeDetail component

---
+++ src/components/user/BadgeDetail.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import { createClient } from "@/utils/supabase/client";
+
+type BadgeDetailProps = {
+  badgeTitle: string;
+};
+
+export default async function BadgeDetail({ badgeTitle }: BadgeDetailProps) {
+  const supabase = await createClient();
+  const { data: badgeImgData } = supabase.storage.from("badge_images").getPublicUrl(`${badgeTitle}.png`);
+  return (
+    <>
+      <div className="flex max-w-sm flex-col items-center gap-1">
+        <Image
+          src={badgeImgData.publicUrl}
+          alt="badge"
+          width={70}
+          height={70}
+          className="border-bg-sub rounded-full border"
+          unoptimized
+        />
+        <p className="text-center font-medium break-keep">훈수톡 입장</p>
+        <p className="text-text-light text-center text-[8px] break-keep">첫 가입</p>
+      </div>
+    </>
+  );
+}
