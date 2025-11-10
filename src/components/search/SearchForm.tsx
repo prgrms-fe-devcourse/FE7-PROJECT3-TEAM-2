147b4b01b1335d299c49268364d6253bc351f3a4
varYeon <xjkooy100@gmail.com>
2025-11-10 14:09:32 +0900

    feat: SearchIntro 과 SearchResult 컴포넌트 생성, cva 정렬 수정, query 추가

--- src/components/search/SearchForm.tsx
+++ src/components/search/SearchForm.tsx
@@ -1,27 +1,40 @@
 "use client";
 
+import { cva } from "class-variance-authority";
 import { useSearchParams } from "next/navigation";
 import SearchBar from "./SearchBar";
+import SearchIntro from "./SearchIntro";
 import SearchRecommend from "./SearchRecommend";
+import SearchResult from "./SearchResult";
+
+const searchFormVariants = cva("flex flex-col gap-4 max-h-[314px] w-full max-w-[697px]", {
+  variants: {
+    searched: {
+      true: "mx-6 mt-5",
+      false: "mx-auto",
+    },
+  },
+  defaultVariants: {
+    searched: false,
+  },
+});
 
 export default function SearchForm() {
   const searchParams = useSearchParams();
   const searchType = searchParams.get("type") === "user" ? "user" : "post";
+  const queryParam = searchParams.get("query");
+
+  const isSearched = !!queryParam;
 
   return (
-    <div className="flex max-h-[314px] w-full max-w-[697px] flex-col gap-4">
-      <div className="flex justify-center font-bold">
-        <p className="text-[24px]">찾고 싶은&nbsp;</p>
-        <p className="text-main text-[24px]"> 훈수</p>
-        <p className="text-[24px]">가 있나요?</p>
-      </div>
-      <p className="text-text-light flex justify-center text-[16px]">
-        찾고자 하는 키워드를 검색하면 관련 훈수 모음들을 찾아드릴게요
-      </p>
+    <div className={searchFormVariants({ searched: isSearched })}>
+      {!isSearched && <SearchIntro />}
 
       <SearchBar searchType={searchType} />
 
-      {searchType === "post" && <SearchRecommend />}
+      {isSearched && <SearchResult searchType={searchType} />}
+
+      {!isSearched && searchType === "post" && <SearchRecommend />}
     </div>
   );
 }
--- src/components/search/SearchRecommend.tsx
+++ src/components/search/SearchRecommend.tsx
@@ -6,10 +6,10 @@ export default function SearchRecommend() {
       <p className="text-main text-[14px]">추천 검색어</p>
       <div className="flex gap-3">
         {/* 더미 */}
-        <Badge size="md" bgColor="#ec4899" textColor="white" text="연애" />
-        <Badge size="md" bgColor="#4858ec" textColor="white" text="친구" />
-        <Badge size="md" bgColor="#45d5b6" textColor="white" text="비트코인" />
-        <Badge size="md" bgColor="#f9b446" textColor="white" text="인간관계" />
+        <Badge size="md" className="bg-pink-500 px-2 py-1 text-white" text="연애" />
+        <Badge size="md" className="bg-emerald-500 px-2 py-1 text-white" text="친구" />
+        <Badge size="md" className="bg-amber-500 px-2 py-1 text-white" text="비트코인" />
+        <Badge size="md" className="bg-cyan-500 px-2 py-1 text-white" text="인간관계" />
       </div>
     </div>
   );
