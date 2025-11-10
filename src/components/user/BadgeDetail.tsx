38ff059279e3ed653266377ead4e27dcac159e65
“moming109” <skhu202013029@gmail.com>
2025-11-10 13:02:37 +0900

    feat: BadgeDetail component 클라이언트 컴포넌트로 변경

--- src/components/user/BadgeDetail.tsx
+++ src/components/user/BadgeDetail.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import Image from "next/image";
+import { useMemo } from "react";
 import { twMerge } from "tailwind-merge";
 import { createClient } from "@/utils/supabase/client";
 
@@ -7,20 +10,26 @@ type BadgeDetailProps = {
   className?: string;
 };
 
-export default async function BadgeDetail({ badgeTitle, className }: BadgeDetailProps) {
-  const supabase = await createClient();
-  const { data: badgeImgData } = supabase.storage.from("badge_images").getPublicUrl(`${badgeTitle}.png`);
+export default function BadgeDetail({ badgeTitle, className }: BadgeDetailProps) {
+  const badgeImgData = useMemo(() => {
+    const supabase = createClient();
+    const { data } = supabase.storage.from("badge_images").getPublicUrl(`${badgeTitle}.png`);
+    return data.publicUrl;
+  }, [badgeTitle]);
+
   return (
     <>
       <div className={twMerge(className, "flex max-w-sm flex-col items-center gap-1")}>
-        <Image
-          src={badgeImgData.publicUrl}
-          alt="badge"
-          width={70}
-          height={70}
-          className="border-bg-sub rounded-full border"
-          unoptimized
-        />
+        {badgeImgData && (
+          <Image
+            src={badgeImgData}
+            alt="badge"
+            width={70}
+            height={70}
+            className="border-bg-sub rounded-full border"
+            unoptimized
+          />
+        )}
         <p className="text-center font-medium break-keep">훈수톡 입장</p>
         <p className="text-text-light text-center text-[8px] break-keep">첫 가입</p>
       </div>
