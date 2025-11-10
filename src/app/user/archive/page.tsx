bd7274297ef83002f300c4d31116aa58f79da49f
“moming109” <skhu202013029@gmail.com>
2025-11-10 12:21:53 +0900

    feat: create archive page UI

--- src/app/user/archive/page.tsx
+++ src/app/user/archive/page.tsx
@@ -1,3 +1,24 @@
+import { AArrowDown } from "lucide-react";
+import ResponsiveContainer from "@/components/common/ResponsiveContainer";
+
 export default function page() {
-  return <div>아카이브 페이지</div>;
+  return (
+    <div className="mt-6.5 w-full text-xs">
+      <ResponsiveContainer className="flex flex-col gap-3 p-6">
+        <div className="flex items-center gap-4">
+          <p className="border-main border-b py-1">내 게시물</p>
+          <p className="text-text-light">북마크</p>
+        </div>
+        <div className="relative flex">
+          <div className="flex items-center gap-2 rounded-full border border-gray-200 px-3 py-2">
+            <AArrowDown size={12} />
+            <select className="outline-none">
+              <option>카테고리</option>
+              <option>작성일</option>
+            </select>
+          </div>
+        </div>
+      </ResponsiveContainer>
+    </div>
+  );
 }
