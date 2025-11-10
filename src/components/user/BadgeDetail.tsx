461acf9fdc2ab7b8e701f6a49343a8325f20d523
“moming109” <skhu202013029@gmail.com>
2025-11-10 12:22:34 +0900

    style: BadgeDetail component className prop 추가

--- src/components/user/BadgeDetail.tsx
+++ src/components/user/BadgeDetail.tsx
@@ -1,16 +1,18 @@
 import Image from "next/image";
+import { twMerge } from "tailwind-merge";
 import { createClient } from "@/utils/supabase/client";
 
 type BadgeDetailProps = {
   badgeTitle: string;
+  className?: string;
 };
 
-export default async function BadgeDetail({ badgeTitle }: BadgeDetailProps) {
+export default async function BadgeDetail({ badgeTitle, className }: BadgeDetailProps) {
   const supabase = await createClient();
   const { data: badgeImgData } = supabase.storage.from("badge_images").getPublicUrl(`${badgeTitle}.png`);
   return (
     <>
-      <div className="flex max-w-sm flex-col items-center gap-1">
+      <div className={twMerge(className, "flex max-w-sm flex-col items-center gap-1")}>
         <Image
           src={badgeImgData.publicUrl}
           alt="badge"
