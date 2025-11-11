b18a3dd0dda1a2be2e9657497614d798991bfc33
“moming109” <skhu202013029@gmail.com>
2025-11-11 09:28:22 +0900

    fix([category]/page): params 임시 받아오기

--- src/app/posts/[category]/page.tsx
+++ src/app/posts/[category]/page.tsx
@@ -1,4 +1,9 @@
-export default async function PostsCategoryPage() {
+type Props = {
+  params: { category: string };
+};
+
+export default async function PostsCategoryPage({ params }: Props) {
   // 카테고리 선택 관련 로직 작성 예정
+  console.log(params);
   return null;
 }
