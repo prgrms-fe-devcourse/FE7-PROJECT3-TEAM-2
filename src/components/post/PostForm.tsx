debe70ef3b2b58bf025c780fd859f2ae9abdeb53
Song YoungJu <moeirs545@gmail.com>
2025-11-11 09:59:04 +0900

    fix: 중복된 posts 폴더 제거

--- src/components/post/PostForm.tsx
+++ src/components/post/PostForm.tsx
@@ -14,9 +14,9 @@ export default function PostForm({ categorys }: { categorys: CategoryType[] }) {
   };
   return (
     <ResponsiveContainer className="post-form bg-bg-main relative flex h-full w-full flex-col overflow-hidden max-sm:border-none">
-      <form onSubmit={handleSubmit} className="flex flex-col justify-between gap-6 px-6 py-5 pt-9">
-        <div className="post-form_rows flex flex-col gap-6">
-          <div className="flex flex-col gap-2">
+      <form onSubmit={handleSubmit} className="flex flex-col justify-between gap-6 px-6 py-5">
+        <div className="post-form_rows flex flex-col gap-4">
+          <div className="flex flex-col gap-1">
             <h1 className="text-text-title flex items-center text-2xl font-bold">
               {" "}
               <button
@@ -31,11 +31,11 @@ export default function PostForm({ categorys }: { categorys: CategoryType[] }) {
             </h1>
             <p className="text-text-light">어떤 글에 훈수를 받고 싶나요?</p>
           </div>
-          <div className="post-form_row flex flex-col gap-2.5">
+          <div className="post-form_row flex flex-col gap-1.5">
             <h2 className="text-text-title text-base">카테고리</h2>
             <CategoryDropdown categorys={categorys} />
           </div>
-          <div className="post-form_row flex flex-col gap-2.5">
+          <div className="post-form_row flex flex-col gap-1.5">
             <h2 className="text-text-title text-base">제목</h2>
             <input
               type="text"
@@ -43,7 +43,7 @@ export default function PostForm({ categorys }: { categorys: CategoryType[] }) {
               className="text-text-sub focus:border-text-sub flex w-full cursor-pointer items-center justify-between rounded-xl border border-gray-200 px-3 py-4 outline-0 placeholder:text-gray-200 hover:cursor-auto"
             />
           </div>
-          <div className="post-form_row flex flex-col gap-2.5">
+          <div className="post-form_row flex flex-col gap-1.5">
             <h2 className="text-text-title text-base">내용</h2>
             <textarea
               placeholder="내용을 입력하세요"
@@ -51,7 +51,7 @@ export default function PostForm({ categorys }: { categorys: CategoryType[] }) {
               className="text-text-sub focus:border-text-sub flex h-60 w-full cursor-pointer resize-none items-center justify-between rounded-xl border border-gray-200 px-3 py-4 outline-0 placeholder:text-gray-200 hover:cursor-auto"
             />
           </div>
-          <div className="post-form_row flex flex-col gap-2.5">
+          <div className="post-form_row flex flex-col gap-1.5">
             <h2 className="text-text-title text-base">이미지 첨부</h2>
             <button className="flex h-36 w-36 cursor-pointer items-center justify-center rounded-xl bg-gray-200 transition-all hover:bg-gray-300">
               <ImagePlus size={40} className="text-gray-400" />
