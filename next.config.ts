ebbd40f7881d1fbba9e84c7d20d496f6aa453447
“moming109” <skhu202013029@gmail.com>
2025-11-11 17:06:53 +0900

    fix: Next Image 도메인 설정

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,12 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/storage/v1/object/public/**",
       },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+        pathname: "/u/**",
+      },
     ],
   },
 };
