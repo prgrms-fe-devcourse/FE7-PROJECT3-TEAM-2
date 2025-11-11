76e465f6bc05c04daf56a6090ebc7047d5f5f1a6
Song YoungJu <moeirs545@gmail.com>
2025-11-11 10:28:24 +0900

    feat: 비로그인 유저 글쓰기 버튼 숨기기 및 /posts/write 접근 차단

--- src/app/(public)/posts/write/page.tsx
+++ src/app/(public)/posts/write/page.tsx
@@ -1,9 +1,19 @@
+import { redirect } from "next/navigation";
 import PostForm from "@/components/post/PostForm";
 import { CategoryType } from "@/types";
-import { createClient } from "@/utils/supabase/client";
+import { createClient } from "@/utils/supabase/server";
 
 export default async function NewPostPage() {
-  const supabase = createClient();
+  const supabase = await createClient();
   const { data } = await supabase.from("category").select("*");
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    alert("로그인이 필요한 기능입니다.");
+    redirect("/login");
+  }
+
   return <PostForm categorys={data as CategoryType[]} />;
 }
--- src/components/post/PostSideBar.tsx
+++ src/components/post/PostSideBar.tsx
@@ -3,11 +3,13 @@
 import { Grid2x2, Plus, Users } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 import { twMerge } from "tailwind-merge";
 import { Button } from "@/components/common/Button";
 import { Divider } from "@/components/common/Divider";
 import ResponsiveContainer from "@/components/common/ResponsiveContainer";
 import PostCardButton from "@/components/post/PostCardButton";
+import { createClient } from "@/utils/supabase/client";
 
 export interface postCardSampleDataType {
   id: string;
@@ -31,6 +33,18 @@ export default function PostSideBar() {
     categoryType: "love",
   };
 
+  const supabase = createClient();
+  const [isLogin, setIsLogin] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+      setIsLogin(!!user);
+    })();
+  });
+
   return (
     <div
       className={twMerge(
@@ -38,24 +52,28 @@ export default function PostSideBar() {
         isPostDetail && "max-[1100px]:hidden"
       )}
     >
-      <Link href="/posts/write" className="max-sm:w-full">
-        <Button size="md" className="w-60 max-sm:w-full">
-          <Plus size={24} />
-          <span>글쓰기</span>
-        </Button>
-      </Link>
-
-      <Divider width="90" />
-      <div className="post-filter-btn flex gap-3 max-sm:w-full">
-        <Button size="sm" className="max-sm:w-full">
-          <Grid2x2 size={12} className="mr-2" />
-          <span>전체보기</span>
-        </Button>
-        <Button size="sm" className="px-3 max-sm:w-full" variant="tertiary">
-          <Users size={12} className="mr-2" />
-          <span>내가 팔로우한 사용자</span>
-        </Button>
-      </div>
+      {isLogin && (
+        <>
+          {" "}
+          <Link href="/posts/write" className="max-sm:w-full">
+            <Button size="md" className="w-60 max-sm:w-full">
+              <Plus size={24} />
+              <span>글쓰기</span>
+            </Button>
+          </Link>
+          <Divider width="90" />
+          <div className="post-filter-btn flex gap-3 max-sm:w-full">
+            <Button size="sm" className="max-sm:w-full">
+              <Grid2x2 size={12} className="mr-2" />
+              <span>전체보기</span>
+            </Button>
+            <Button size="sm" className="px-3 max-sm:w-full" variant="tertiary">
+              <Users size={12} className="mr-2" />
+              <span>내가 팔로우한 사용자</span>
+            </Button>
+          </div>
+        </>
+      )}
       <ResponsiveContainer className="px-3 py-4.5 max-sm:w-full">
         <PostCardButton data={postCardSampleData} className="max-sm:w-full" />
         <PostCardButton data={postCardSampleData} className="max-sm:w-full" />
