790a3541ad5e8f27f1becf3c29bd48d239ec3588
Song YoungJu <moeirs545@gmail.com>
2025-11-13 00:31:37 +0900

    feat: 게시글 선택 안 했을 때 컴포넌트 렌더링

--- src/app/(public)/posts/[category]/page.tsx
+++ src/app/(public)/posts/[category]/page.tsx
@@ -1,3 +1,9 @@
+import ResponsiveContainer from "@/components/common/ResponsiveContainer";
+
 export default function PostsCategoryPage() {
-  return null;
+  return (
+    <ResponsiveContainer className="bg-bg-main text-text-light flex w-full items-center justify-center max-sm:hidden">
+      <span>선택된 게시글이 없습니다.</span>
+    </ResponsiveContainer>
+  );
 }
