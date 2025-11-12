8ffab3a0d49eab313fac006ce89be7f3961bc4d0
leehyunjun97 <monsterstamp79@gmail.com>
2025-11-12 12:47:02 +0900

    feat: 수파베이스 연동 카테고리별차트 카드 출력

--- src/components/chart/categoryRank/CategoryRankCard.tsx
+++ src/components/chart/categoryRank/CategoryRankCard.tsx
@@ -2,22 +2,30 @@ import testImg from "@/assets/images/navbar/profile.png";
 import Badge from "@/components/common/Badge";
 import CircleProfileImage from "@/components/common/image/CircleProfileImage";
 import ResponsiveContainer from "@/components/common/ResponsiveContainer";
+import { CategoryType } from "@/types";
+import { categoryColor } from "@/utils/category";
 import AdoptRatioChart from "./AdoptRatioChart";
 import ChartFillPie from "../ChartFillPie";
 
 export default function CategoryRankCard({
+  data,
   stats,
 }: {
-  stats: {
+  data: {
     name: string;
     value: number;
   }[];
+  stats: CategoryType;
 }) {
+  const color = categoryColor[stats.name];
+
   return (
     <ResponsiveContainer className="col-span-1 flex min-h-[360px] flex-col gap-8 p-6 whitespace-nowrap md:gap-12">
       <div className="flex flex-col gap-2">
         <span className="text-text-light text-sm font-semibold">카테고리</span>
-        <span className="text-lg font-semibold text-pink-500">연애</span>
+        <span className="font-semibol text-lg" style={{ color }}>
+          {stats.name}
+        </span>
       </div>
       <div className="flex w-full flex-wrap gap-12">
         <div className="flex w-full max-w-[200px] flex-col gap-2">
@@ -35,41 +43,41 @@ export default function CategoryRankCard({
         <div className="flex flex-col gap-1">
           <div className="text-text-title mb-1 text-sm font-semibold">키워드 TOP3</div>
           <div className="flex gap-5">
-            <Badge size="md" className="bg-pink-500 px-2 py-1 text-white" text="사랑" />
-            <Badge size="md" className="bg-pink-500 px-2 py-1 text-white" text="이별" />
-            <Badge size="md" className="bg-pink-500 px-2 py-1 text-white" text="남자친구" />
+            <Badge size="md" className="px-2 py-1 text-white" text="사랑" style={{ backgroundColor: color }} />
+            <Badge size="md" className="px-2 py-1 text-white" text="사랑" style={{ backgroundColor: color }} />
+            <Badge size="md" className="px-2 py-1 text-white" text="사랑" style={{ backgroundColor: color }} />
           </div>
         </div>
       </div>
       <div className="flex w-full flex-col gap-2">
         <div className="text-text-title mb-2 text-sm font-semibold">랭킹 TOP3</div>
         <div className="grid max-w-[360px] cursor-pointer grid-cols-3 gap-8">
-          <div className="col-span-1 flex flex-col items-center justify-center gap-2 rounded-xl bg-[#F9E79F] p-3 px-4">
+          <div className="col-span-1 flex flex-col items-center justify-center gap-2 rounded-xl border border-[#F9E79F] p-3 px-4">
             <CircleProfileImage src={testImg} size="md" className="border-none" />
             <span className="text-text-content font-semibol text-xs">내이름훈수왕</span>
           </div>
-          <div className="col-span-1 flex flex-col items-center justify-center gap-2 rounded-xl bg-[#D6DBDF] p-3 px-4">
+          <div className="col-span-1 flex flex-col items-center justify-center gap-2 rounded-xl border border-[#D6DBDF] p-3 px-4">
             <CircleProfileImage src={testImg} size="md" className="border-none" />
             <span className="text-text-content text-xs font-semibold">꼰대</span>
           </div>
-          <div className="col-span-1 flex flex-col items-center justify-center gap-2 rounded-xl bg-[#F5CBA7] p-3 px-4">
+          <div className="col-span-1 flex flex-col items-center justify-center gap-2 rounded-xl border border-[#F5CBA7] p-3 px-4">
             <CircleProfileImage src={testImg} size="md" className="border-none" />
             <span className="text-text-content text-xs font-semibold">훈장님</span>
           </div>
         </div>
       </div>
-      <div className="flex flex-wrap gap-10">
-        <div className="flex flex-col gap-2">
+      <div className="flex w-full flex-wrap gap-10">
+        <div className="flex w-full max-w-[250px] flex-col gap-2">
           <div className="text-text-title mb-2 text-sm font-semibold">지표</div>
           <div className="flex gap-7">
-            <AdoptRatioChart label="채택률" percentage={60} color="#f6339a" />
-            <AdoptRatioChart label="평균 뱃지" percentage={3} color="#f6339a" />
+            <AdoptRatioChart label="채택률" percentage={60} color={color} />
+            <AdoptRatioChart label="사용자별 뱃지" percentage={3} color={color} />
           </div>
         </div>
         <div className="flex flex-col gap-3">
           <div className="text-text-title text-sm font-semibold">업적별 뱃지</div>
           <div className="h-[130px]">
-            <ChartFillPie stats={stats} />
+            <ChartFillPie stats={data} />
           </div>
         </div>
       </div>
--- src/components/chart/categoryRank/CategoryRankComponent.tsx
+++ src/components/chart/categoryRank/CategoryRankComponent.tsx
@@ -1,19 +1,32 @@
+import { createClient } from "@/utils/supabase/server";
 import CategoryRankCard from "./CategoryRankCard";
 
-export default function CategoryRankComponent({
-  stats,
-}: {
-  stats: {
-    name: string;
-    value: number;
-  }[];
-}) {
+export default async function CategoryRankComponent() {
+  const badgeData = [
+    { name: "연애", value: 25 },
+    { name: "기술/IT", value: 18 },
+    { name: "제테크/소비", value: 10 },
+    { name: "음식/요리", value: 9 },
+    { name: "생활", value: 8 },
+    { name: "게임", value: 7 },
+  ];
+
+  const supabase = await createClient();
+
+  const { data: categoryTotalData, error: categoryDataError } = await supabase.from("category").select("*");
+  const { data: topKeyword, error: topKeywordError } = await supabase.rpc("get_top_keyword");
+
+  console.log(topKeyword);
+  console.log(categoryTotalData);
+
+  // 에러처리
+  if (categoryDataError) console.log(categoryTotalData);
+
   return (
     <div className="grid w-full grid-cols-1 gap-5 py-7 pt-0 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2">
-      <CategoryRankCard stats={stats} />
-      <CategoryRankCard stats={stats} />
-      <CategoryRankCard stats={stats} />
-      <CategoryRankCard stats={stats} />
+      {categoryTotalData?.map(stats => (
+        <CategoryRankCard key={stats.id} data={badgeData} stats={stats} />
+      ))}
     </div>
   );
 }
