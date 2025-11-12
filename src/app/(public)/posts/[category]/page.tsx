31ad9ca547c51fce23bc29f8dabfdc89c42754ab
Song YoungJu <moeirs545@gmail.com>
2025-11-13 01:01:53 +0900

    refactor: 게시글 없을 시 PostSideList, PostCard 컴포넌트 내용 통일감 있게 수정

--- src/app/(public)/posts/[category]/page.tsx
+++ src/app/(public)/posts/[category]/page.tsx
@@ -2,8 +2,8 @@ import ResponsiveContainer from "@/components/common/ResponsiveContainer";
 
 export default function PostsCategoryPage() {
   return (
-    <ResponsiveContainer className="bg-bg-main text-text-light flex w-full items-center justify-center max-sm:hidden">
-      <span>선택된 게시글이 없습니다.</span>
+    <ResponsiveContainer className="bg-bg-main flex w-full items-center justify-center max-sm:hidden">
+      <span className="text-text-light text-sm">선택된 게시글이 없습니다.</span>
     </ResponsiveContainer>
   );
 }
--- src/components/post/PostSideList.tsx
+++ src/components/post/PostSideList.tsx
@@ -1,14 +1,17 @@
+import { twMerge } from "tailwind-merge";
 import { PostCardType } from "@/types";
 import PostCardButton from "./PostCardButton";
 import ResponsiveContainer from "../common/ResponsiveContainer";
 
 export default function PostSideList({ postData }: { postData: PostCardType[] }) {
   return (
-    <ResponsiveContainer className="w-full px-3 py-4.5">
+    <ResponsiveContainer
+      className={twMerge("w-full px-3 py-4.5", postData.length === 0 && "flex h-full items-center justify-center")}
+    >
       {postData.length !== 0 ? (
         postData.map(post => <PostCardButton key={post.id} postData={post} className="max-sm:w-full" />)
       ) : (
-        <p className="text-text-light text-center align-middle text-sm">게시글이 없습니다.</p>
+        <span className="text-text-light text-sm">작성된 게시글이 없습니다.</span>
       )}
     </ResponsiveContainer>
   );
