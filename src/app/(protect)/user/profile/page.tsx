6976ad293ceec36f5271b772a86239cd668483dc
“moming109” <skhu202013029@gmail.com>
2025-11-13 09:04:51 +0900

    feat(user/profile): userInfo 분리

--- src/app/(protect)/user/profile/page.tsx
+++ src/app/(protect)/user/profile/page.tsx
@@ -1,48 +1,90 @@
-import { ActivityIcon, ArrowRightToLineIcon, Award, ChartLine, UserRound, UserRoundPen } from "lucide-react";
+import { ActivityIcon, ArrowRightToLineIcon, Award, ChartLine, UserRound } from "lucide-react";
 import Link from "next/link";
-import Badge from "@/components/common/Badge";
-import { Button } from "@/components/common/Button";
+import { redirect } from "next/navigation";
 import { Divider } from "@/components/common/Divider";
-import CircleProfileImage from "@/components/common/image/CircleProfileImage";
 import ResponsiveContainer from "@/components/common/ResponsiveContainer";
 import BadgeDetail from "@/components/user/BadgeDetail";
 import CategoryRanking from "@/components/user/CategoryRanking";
+import UserInfo from "@/components/user/UserInfo";
+import { updateProfile } from "@/services/profile/updateProfile";
+import { FormState } from "@/types";
+import { createClient } from "@/utils/supabase/server";
 
-export default function page() {
+export default async function page() {
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    redirect("/auth/login");
+  }
+
+  const { data: profile } = await supabase.from("profiles").select("*").eq("id", user.id).single();
+
+  const { count: followsCount } = await supabase
+    .from("follow")
+    .select("*", { count: "exact", head: true })
+    .eq("follower_id", user.id);
+
+  const { count: followersCount } = await supabase
+    .from("follow")
+    .select("*", { count: "exact", head: true })
+    .eq("following_id", user.id);
+
+  const { count: postsCount } = await supabase
+    .from("posts")
+    .select("*", { count: "exact", head: true })
+    .eq("user_id", user.id);
+
+  // const { data: adoptedComments } = await supabase
+  //   .from("comments")
+  //   .select("posts!inner(adopted_comment_id)")
+  //   .eq("user_id", user.id);
+  // console.log(adoptedComments);
+
+  // 서버 액션 정의 (폼이 이 함수를 호출하게 됨)
+  async function updateProfileAction(prevState: FormState, formData: FormData): Promise<FormState> {
+    "use server";
+
+    const supabase = await createClient();
+    const {
+      data: { user },
+      error: userError,
+    } = await supabase.auth.getUser();
+
+    if (!user || userError) {
+      return {
+        success: false,
+        error: "로그인이 필요합니다.",
+      };
+    }
+
+    const name = formData.get("name")?.toString() ?? "";
+    const email = formData.get("email")?.toString() ?? "";
+    const phoneNumber = formData.get("phoneNumber")?.toString().replaceAll(" ", "").replaceAll(/[\D]/gi, "") ?? "";
+    const bio = formData.get("bio")?.toString() ?? "";
+
+    if (!name || !email) {
+      return {
+        success: false,
+        error: "필수 항목을 채워주세요",
+      };
+    }
+
+    if (phoneNumber.length !== 8 && phoneNumber.length !== 11) {
+      return {
+        success: false,
+        error: "올바른 전화번호를 입력해주세요.",
+      };
+    }
+
+    return updateProfile(user.id, name, email, phoneNumber, bio);
+  }
+
   return (
     <div className="mt-6.5 w-full pb-5 text-xs">
-      <div className="p-6">
-        <div className="mb-3 flex items-center gap-3">
-          <CircleProfileImage src={""} size="lg" />
-          <div className="flex flex-col gap-2">
-            <Badge size="sm" text="LV.1" className="bg-main text-white" />
-            <p className="font-medium">김철수</p>
-          </div>
-        </div>
-        <div className="flex flex-col gap-6 text-xs">
-          <p className="text-text-light">개인정보</p>
-          <div className="flex flex-wrap gap-56 max-sm:flex-col max-sm:gap-8">
-            <div>
-              <p className="text-text-sub mb-3">이메일</p>
-              <p>kimch@example.com</p>
-            </div>
-            <div>
-              <p className="text-text-sub mb-3">전화번호</p>
-              <p>010-9999-9999</p>
-            </div>
-            <div>
-              <p className="text-text-sub mb-3">한줄 소개</p>
-              <p>안녕하세요</p>
-            </div>
-          </div>
-          <div className="flex justify-end">
-            <Button size={"xs"} variant={"secondary"}>
-              <UserRoundPen size={12} className="mr-1" />
-              수정하기
-            </Button>
-          </div>
-        </div>
-      </div>
+      <UserInfo profile={profile} action={updateProfileAction} />
       <Divider />
       <div className="my-6 flex flex-wrap justify-center gap-3 text-xs">
         <ResponsiveContainer className="flex flex-col gap-12 p-6 max-sm:gap-14 max-sm:rounded-3xl">
@@ -54,11 +96,11 @@ export default function page() {
             <div className="flex gap-6">
               <div className="flex min-w-32.5 flex-col gap-2 rounded-xl border border-gray-200 px-3.5 pt-3 pb-2 max-sm:min-w-25">
                 <p>팔로우</p>
-                <p className="text-base">120</p>
+                <p className="text-base">{followsCount}</p>
               </div>
               <div className="flex min-w-32.5 flex-col gap-2 rounded-xl border border-gray-200 px-3.5 pt-3 pb-2 max-sm:min-w-25">
                 <p>팔로잉</p>
-                <p className="text-base">120</p>
+                <p className="text-base">{followersCount}</p>
               </div>
             </div>
           </div>
@@ -70,16 +112,16 @@ export default function page() {
             <div className="flex gap-6">
               <div className="bg-bg-sub flex min-w-32.5 flex-col gap-2 rounded-xl px-3.5 pt-3 pb-2 max-sm:min-w-25">
                 <p>게시물</p>
-                <p className="text-base">120</p>
+                <p className="text-base">{postsCount}</p>
               </div>
               <div className="bg-bg-sub flex min-w-32.5 flex-col gap-2 rounded-xl px-3.5 pt-3 pb-2 max-sm:min-w-25">
                 <p>채택 수</p>
-                <p className="text-base">24</p>
+                <p className="text-base"></p>
               </div>
             </div>
           </div>
         </ResponsiveContainer>
-        <ResponsiveContainer className="flex-1 p-6 max-sm:rounded-3xl max-sm:pb-3">
+        <ResponsiveContainer className="flex-1 p-6 max-lg:min-w-63 max-sm:min-w-0 max-sm:rounded-3xl max-sm:pb-3">
           <div className="flex flex-col gap-6 max-sm:gap-3">
             <div className="flex flex-col gap-2">
               <div className="flex items-center gap-1">
@@ -89,11 +131,11 @@ export default function page() {
               <p className="text-text-light">대표로 설정한 4개의 뱃지만 표시</p>
             </div>
 
-            <div className="flex justify-center gap-16 max-sm:grid max-sm:grid-cols-2 max-sm:gap-1">
-              <BadgeDetail badgeTitle="basic_welcome" />
-              <BadgeDetail badgeTitle="basic_welcome" />
-              <BadgeDetail badgeTitle="basic_welcome" />
-              <BadgeDetail badgeTitle="basic_welcome" />
+            <div className="flex flex-wrap justify-center gap-16 max-sm:grid max-sm:grid-cols-2 max-sm:gap-1">
+              <BadgeDetail badgeTitle="basic_welcome" className="shrink-0" />
+              <BadgeDetail badgeTitle="basic_welcome" className="shrink-0" />
+              <BadgeDetail badgeTitle="basic_welcome" className="shrink-0" />
+              <BadgeDetail badgeTitle="basic_welcome" className="shrink-0" />
             </div>
             <Link href="badge" className="text-main flex items-center justify-end gap-2 px-3 py-1.5 text-xs">
               <ArrowRightToLineIcon size={10} />
