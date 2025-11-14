5eef1d5de617f35011d96ddc7e8a485aede401bb
“moming109” <skhu202013029@gmail.com>
2025-11-14 17:41:15 +0900

    fix(BadgeDetail): Image quality 속성 추가 및 구조 변경

--- src/components/user/BadgeDetail.tsx
+++ src/components/user/BadgeDetail.tsx
@@ -1,37 +1,37 @@
 "use client";
 
 import Image from "next/image";
-import { useMemo } from "react";
 import { twMerge } from "tailwind-merge";
-import { createClient } from "@/utils/supabase/client";
 
 type BadgeDetailProps = {
-  badgeTitle: string;
+  badgeData: {
+    badge_image: string | null;
+    category_id: string;
+    desc: string;
+    id: string;
+    name: string;
+    point: number | null;
+  };
   className?: string;
 };
 
-export default function BadgeDetail({ badgeTitle, className }: BadgeDetailProps) {
-  const badgeImgData = useMemo(() => {
-    const supabase = createClient();
-    const { data } = supabase.storage.from("badge_images").getPublicUrl(`${badgeTitle}.png`);
-    return data.publicUrl;
-  }, [badgeTitle]);
-
+export default function BadgeDetail({ badgeData, className }: BadgeDetailProps) {
   return (
     <>
-      <div className={twMerge(className, "flex max-w-sm flex-col items-center gap-1")}>
-        {badgeImgData && (
+      <div className={twMerge(className, "flex w-20 flex-col items-center gap-1")}>
+        {badgeData.badge_image && (
           <Image
-            src={badgeImgData}
-            alt="badge"
+            src={badgeData.badge_image}
+            alt={badgeData.name}
             width={70}
             height={70}
             className="border-bg-sub rounded-full border"
-            unoptimized
+            loading="eager"
+            quality={100}
           />
         )}
-        <p className="text-center font-medium break-keep">훈수톡 입장</p>
-        <p className="text-text-light text-center text-[8px] break-keep">첫 가입</p>
+        <p className="text-center font-medium break-keep">{badgeData.name}</p>
+        <p className="text-text-light text-center text-[8px] break-keep">{badgeData.desc}</p>
       </div>
     </>
   );
