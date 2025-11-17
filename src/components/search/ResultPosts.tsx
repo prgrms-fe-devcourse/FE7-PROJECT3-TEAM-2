c80e79261a50e38b27f12b3464dff29c41b1694a
varYeon <xjkooy100@gmail.com>
2025-11-17 18:17:27 +0900

    feat: 작성 게시글 없을 때 조건 처리

--- src/components/search/ResultPosts.tsx
+++ src/components/search/ResultPosts.tsx
@@ -11,7 +11,7 @@ export default function ResultPosts({
   searchData,
   textHighlight,
 }: {
-  searchData: PostWithProfile[];
+  searchData: PostWithProfile[] | null | undefined;
   textHighlight?: (text: string) => string | React.ReactElement[];
 }) {
   const router = useRouter();
@@ -19,30 +19,40 @@ export default function ResultPosts({
     router.push(`/posts/${category}/post/${id}`);
   };
 
-  return searchData?.map(post => (
-    <div key={post.id} className={cardVariants()} onClick={() => handleClick(post.id, post.category?.type)}>
-      <div className="flex flex-col gap-6 p-6">
-        <div className="flex gap-2 text-xs">
-          <p>{post.profiles?.name}</p>
-          <p className="text-text-light">{post.created_at}</p>
-        </div>
-        <div className="flex flex-col gap-2.5 text-sm">
-          <p className="font-bold">{textHighlight ? textHighlight(post.title) : post.title}</p>
-          <p className="text-text-light">{textHighlight ? textHighlight(post.content ?? "") : (post.content ?? "")}</p>
-        </div>
-        <Badge size="sm" className="bg-pink-600 px-2 py-1 text-white" text="HOT" />
-      </div>
-      <div>
-        {post.post_image && (
-          <Image
-            src={post.post_image}
-            alt={`${post.title}의 썸네일`}
-            width={192}
-            height={192}
-            className="m-1.5 h-48 min-h-48 w-48 min-w-48 rounded-sm"
-          />
-        )}
-      </div>
-    </div>
-  ));
+  return (
+    <>
+      {(!searchData || searchData.length === 0) && (
+        <p className="flex justify-center py-3 text-sm">첫 훈수 요청을 해보세요</p>
+      )}
+      {searchData &&
+        searchData.map(post => (
+          <div key={post.id} className={cardVariants()} onClick={() => handleClick(post.id, post.category?.type)}>
+            <div className="flex flex-col gap-6 p-6">
+              <div className="flex gap-2 text-xs">
+                <p>{post.profiles?.name}</p>
+                <p className="text-text-light">{post.created_at}</p>
+              </div>
+              <div className="flex flex-col gap-2.5 text-sm">
+                <p className="font-bold">{textHighlight ? textHighlight(post.title) : post.title}</p>
+                <p className="text-text-light">
+                  {textHighlight ? textHighlight(post.content ?? "") : (post.content ?? "")}
+                </p>
+              </div>
+              <Badge size="sm" className="bg-pink-600 px-2 py-1 text-white" text="HOT" />
+            </div>
+            <div>
+              {post.post_image && (
+                <Image
+                  src={post.post_image}
+                  alt={`${post.title}의 썸네일`}
+                  width={192}
+                  height={192}
+                  className="m-1.5 h-48 min-h-48 w-48 min-w-48 rounded-sm"
+                />
+              )}
+            </div>
+          </div>
+        ))}
+    </>
+  );
 }
