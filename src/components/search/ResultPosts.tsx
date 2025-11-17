030476cb22cc8400365fce2aefc6adf1b6f3feac
varYeon <xjkooy100@gmail.com>
2025-11-17 18:00:51 +0900

    fix: 아카이브 오류 임시 수정

--- src/components/search/ResultPosts.tsx
+++ src/components/search/ResultPosts.tsx
@@ -19,7 +19,7 @@ export default function ResultPosts({
     router.push(`/posts/${category}/post/${id}`);
   };
 
-  return searchData.map(post => (
+  return searchData?.map(post => (
     <div key={post.id} className={cardVariants()} onClick={() => handleClick(post.id, post.category?.type)}>
       <div className="flex flex-col gap-6 p-6">
         <div className="flex gap-2 text-xs">
