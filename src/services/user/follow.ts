a477d499e5ba0874c56f2f9d77e0022cd43f52a7
SeHyun <rla6170@naver.com>
2025-11-18 10:11:20 +0900

    fix: follow select table

--- src/services/user/follow.ts
+++ src/services/user/follow.ts
@@ -4,17 +4,11 @@ import { createClient } from "@/utils/supabase/client";
 export const getFollowingUserId = async (id: string): Promise<string[]> => {
   const supabase = await createClient();
 
-  const { data, error } = await supabase
-    .from("follow")
-    .select<"following_id", FollowType["Row"]>("following_id")
-    .eq("follower_id", id);
+  const { data, error } = await supabase.from("follow").select<"*", FollowType["Row"]>("*").eq("follower_id", id);
 
   if (error) {
     console.error("Error fetching following:", error);
     return [];
   }
-
-  const followingIds = data?.map(row => row["follower_id"]) ?? [];
-
-  return followingIds;
+  return data.map(f => f.following_id) ?? [];
 };
